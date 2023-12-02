98ae55d4a2a7a6bcfca0ba62700f1640cfd6eccb
Yuri Gatskovskyi <yurimoreau@gmail.com>
2023-12-02 18:53:01 +0200

    make build

---
+++ index.6c9d3091.js
@@ -0,0 +1,2 @@
+var e=document.body,c=document.querySelector(".page__menu");document.querySelector(".icon--hamburger").addEventListener("click",function(){e.classList.add("unscroll")}),c.addEventListener("click",function(){e.classList.remove("unscroll")});
+//# sourceMappingURL=index.6c9d3091.js.map
